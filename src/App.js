28eb90847456d4c1a24753ddcff1bd6d470f0698
Carlos Daniel Hernandez <carlos.h.zuniga45@gmail.com>
2021-09-15 02:11:39 -0600

    Fix code format and clean code

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
-import './App.css';
-import Form from "./components/Form"
+import "./App.css";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
       {
-      <div className="login-container">
-        <div className="login-content">
-          <div className="login-form">
-            <div className="forms-content">
-              <h1>Let's set up your account</h1>
-              <div className="signIn-quote">
-                <p>Already have an account?</p>
-                <h3>Sign in</h3>
-              </div>
-              <Form/>
-              <div className="terms-quote">
-                <p>By clicking the "Next" button, you agree to creating a free account, and to <span>Terms of Services</span> and <span>Privacy Policy.</span></p>  
+        <div className="login-container">
+          <div className="login-content">
+            <div className="login-form">
+              <div className="forms-content">
+                <h1>Let's set up your account</h1>
+                <div className="signIn-quote">
+                  <p>Already have an account?</p>
+                  <h3>Sign in</h3>
+                </div>
+                <Form />
+                <div className="terms-quote">
+                  <p>
+                    By clicking the "Next" button, you agree to creating a free
+                    account, and to <span>Terms of Services</span> and{" "}
+                    <span>Privacy Policy.</span>
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="login-description">
-            <h1>
-              Dummy Heading
-            </h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Sunt architecto maiores aliquid sapiente doloribus.
-              Ducimus consequuntur quae itaque molestiae ratione!
-            </p>
+            <div className="login-description">
+              <h1>Dummy Heading</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+                architecto maiores aliquid sapiente doloribus. Ducimus
+                consequuntur quae itaque molestiae ratione!
+              </p>
+            </div>
           </div>
         </div>
-      </div>
       }
     </div>
   );
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,30 +1,30 @@
 import { useState } from "react";
-function Dropdown ({selected , setSelected}){
-    const [isActive, setIsActive] = useState(false);
-    const options = ['Developer' , 'Designer' , 'Architect']
-    return(
-        <div className="dropdown">
-            <div className="dropdown-btn" onClick={(e) => setIsActive(!isActive)}>
-                <p>
-                {selected}
-                </p>
-                <span className="fas fa-caret-down"></span>
+function Dropdown({ selected, setSelected }) {
+  const [isActive, setIsActive] = useState(false);
+  const options = ["Developer", "Designer", "Architect"];
+  return (
+    <div className="dropdown">
+      <div className="dropdown-btn" onClick={(e) => setIsActive(!isActive)}>
+        <p>{selected}</p>
+        <span className="fas fa-caret-down"></span>
+      </div>
+      {isActive && (
+        <div className="dropdown-content">
+          {options.map((option) => (
+            <div
+              className="dropdown-item"
+              onClick={(e) => {
+                setSelected(option);
+                setIsActive(false);
+              }}
+            >
+              {option}
             </div>
-            {isActive && (
-                <div className="dropdown-content">
-                    {options.map(option => (
-                        <div className="dropdown-item" onClick={e => {
-                            setSelected(option)
-                            setIsActive(false)
-                            }
-                        }>
-                        {option}
-                        </div>
-                    ))}
-            </div>
-            )}
+          ))}
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
 export default Dropdown;
